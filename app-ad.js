be6248549e2aced6f5ced4c88c3ac4c08ea5d395
Harikrishnan R U <hari@Harikrishnans-MacBook-Pro.local>
2020-10-02 15:20:18 +0530

    modified

---
+++ app-ad.js
@@ -0,0 +1,21 @@
+function allotment(){
+    console.log("done");
+    const allotment = document.querySelector('#allotment');
+    const plus1 = document.querySelector('.plus1');
+    const minus1 = document.querySelector('.minus1');
+
+        allotment.classList.toggle("active");
+        plus1.classList.toggle("inactive");
+        minus1.classList.toggle("active");
+}
+
+function lastrank(){
+    console.log("done");
+    const lastrank = document.querySelector('#lastrank');
+    const plus2 = document.querySelector('.plus2');
+    const minus2 = document.querySelector('.minus2');
+
+        lastrank.classList.toggle("active");
+        plus2.classList.toggle("inactive");
+        minus2.classList.toggle("active");
+}
